7ff668092440423430be0cd9b5ffc8ea20f18387
Ayim William <odhisayim@gmail.com>
2024-08-03 23:27:44 +0300

    updated interface

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import './App.css'
+// import './App.scss';
+import './App.min.css';
 
 function App() {
      const [photoGalleryArray, updatePhotoGalleryArray] = useState([]);
@@ -19,24 +20,22 @@ function App() {
      }, [])
      return (
         <div className="App">
-            <nav className="navbar navbar-dark bg-dark">
-                <div className="w-100 text-light">Image Gallery</div>
-            </nav>
-            <div className="row">
+            <h1 className="title">Image Gallery</h1>
+            <div className="gallery">
                 {
                     photoGalleryArray.map((photoGalleryArrayItem, index) => {
                         return (
                             <div key={index} className="col-lg-4 col-md-4 col-sm-12 p-1">
                                 <img src={photoGalleryArrayItem.img_url}
                                     alt={`image_${photoGalleryArrayItem.name}`}
-                                    height="300"
-                                    width="400"
+                                    
                                 />
                             </div>
                         );
                     })
                 }
             </div>
+
         </div>
      );
 }
